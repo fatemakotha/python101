d290880d5fd1258d476316ff7a7294f60996d598
fatemakotha <fatema.alam.kotha@gmail.com>
2021-11-12 14:44:10 +0600

    commit one

---
+++ Print_multi_line_document.py
@@ -0,0 +1,11 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
+
+

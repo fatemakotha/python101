2658a2b85d5fff983ea87c132c104594c6a56ce6
fatemakotha <fatema.alam.kotha@gmail.com>
2021-11-13 21:50:37 +0600

    Adding IS to the beginning of a string if IS is not already there

--- Adding IS to the beginning of a string if IS is not already there.py
+++ Adding IS to the beginning of a string if IS is not already there.py
@@ -1,6 +1,3 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
 #Write a Python program to get a new string from a given string where "Is" has been added to the front. If the given string already begins with "Is" then return the string unchanged.
 def new_string(str):
   if len(str) >= 2 and str[:2] == "Is":

9f1cd3becc211ff200c367296915e73a117ec5a3
fatemakotha <63168025+fatemakotha@users.noreply.github.com>
2021-11-15 23:01:31 +0600

    Alternative method added.

--- Write a Python program to calculate the sum of three given numbers, if the values are equal then return thrice of their sum.py
+++ Write a Python program to calculate the sum of three given numbers, if the values are equal then return thrice of their sum.py
@@ -1,3 +1,5 @@
+#Method_1
+
 def sum_thrice(x, y, z):
 
      sum = x + y + z
@@ -9,3 +11,15 @@ def sum_thrice(x, y, z):
 print(sum_thrice(1, 2, 3))
 print(sum_thrice(3, 3, 3))
 
+#Method_2
+
+x = int(input())
+y = int(input())
+z = int(input())
+
+sum = x+y+z
+ex = (x+y+z)*3
+if x==y==x:
+    print(ex)
+else:
+    print(sum)
